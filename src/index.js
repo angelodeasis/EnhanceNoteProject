b5d9b35e9fdb50e89f69415519db32632f8b9102
Angelo <adeasis@randolphcollege.edu>
2025-02-24 12:50:01 -0500

    Fixed backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require("express"); // express model imported
 const pasth = require("path"); // built-in path module imported
 const bcrypt = require("bcrypt"); // bcrypt model imported
 const collection = require("./config"); 
+const backend = require("./backend");
+const multer = require("multer")
+
 
 const app = express(); // Creates express application
 
@@ -33,6 +36,14 @@ app.get('/pricing', (req, res) => { // Sign-up request/respond function
     res.render("pricing");
 });
 
+
+// File upload
+app.post('/home', async (req, res) => { 
+    var pdf;
+    pdf = $("uploader").val();
+
+
+});
 
 // Registering User
 app.post('/signup', async (req, res) => {
@@ -80,68 +91,6 @@ app.post("/login", async (req, res) => {
     }
 
 });
-
-
-
-app.post('/home', async (req, res) => {
-  let pdf = home.getElementByID("uploader");
-
-  const groq = new Groq({
-    apiKey: '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',
-  });
-
-  async function runModel() {
-    try {
-      const chatCompletion = await groq.chat.completions.create({
-        model: 'mixtral-8x7b-32768',
-        messages: [{ role: 'user', content: 'Tell me a joke.' }],
-      });
-      console.log(chatCompletion.choices[0].message.content);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
-
-  runModel();
-
-  function qna(str) {
-    let qArray = [];
-    let aArray = [];
-    let string;
-    let output;
-    // Removing question and answer labels (python holdover)
-    str = str.replaceAll("Q:", "");
-    str = str.replaceAll("A:", "");
-    // Splitting string at newlines
-    string = str.split("\n");
-    // Pushing questions to question list, other strings to answer list
-    for (let i = 0; i < string.length; i++) {
-      if (string[i].includes("?")){
-        qArray.push(string[i]);
-      }
-      else {
-        aArray.push(string[i])
-      }
-    }
-    // Removing potential label strings from answer list
-    aArray.splice(0, 1);
-    //aArray.splice(aArray.length - 1, 1)
-    output = [qArray, aArray];
-    return output;
-  }
-
-  let inputString = "Here are the study questions and answers based on the notes:\nQ: How does the film portray the human cost of war, particularly through the lens of children?\nA: The film portrays the human cost of war brutally and realistically, showing the intense shock and trauma of losing family at a young age.\nQ: What do we see through the lens of children in the film?\nA: We see the unnecessary death toll of war, as well as the possibility of losing family without warning or preparation, which is life-changing for young children."
-  let qList;
-  let aList;
-  let arrayOut;
-  arrayOut = qna(inputString)
-  qList = arrayOut[0];
-  aList = arrayOut[1];
-  //console.log(arrayOut);
-  console.log(qList);
-  console.log(aList);
-});
-
 
 
 
